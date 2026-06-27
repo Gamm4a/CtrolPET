ea8d7a375796fcbd74a7f71882b0ec7371e97707
Gamm4a <gamael77c11@gmail.com>
2026-06-26 19:33:36 -0700

    Clases duplicadas eliminadas

--- CtrolPET/src/main/java/repository/DuenoRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import model.Dueno;
-import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DuenoRepository extends MongoRepository<Dueno, ObjectId> {
-}
